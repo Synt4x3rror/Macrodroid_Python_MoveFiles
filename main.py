b87734187a0f09123725f8e01c3c10a66fd9907f
Synt4x3rror <synt4x3rror@live.com>
2023-12-29 05:53:01 -0400

    Verison 1.0.0 of Copymaster script, mainly used for copying files (images) in android

--- main.py
+++ main.py
@@ -1,35 +1,28 @@
 import os
-import pathlib
-import sys
+import shutil
+import argparse
 
 def main():
-    setup_process = True
-    # List hard drives and capacity using lsblk (Prerequisite)
+    parser = argparse.ArgumentParser("Copies moves files from source directory to a target directory in a NON RECURSIVE way (No directory nesting).")
+    parser.add_argument("source_dir", help="Input Directory", type=str)
+    parser.add_argument("target_dir", help="Target Directory", type=str)
+    args = parser.parse_args()
 
-    # Setup devices that will be copied from/to
-    while(setup_process):
-        os.system('clear')
-        source, target = __target_select()
-        print(f"Use {source} as Source and {target} as Target?")
-        confirm = input("[Y]es\t|\t[N]o\t|\t[Q]uit\n")
-        if(confirm.upper() == 'Q'):
-            sys.exit('Exit script. No changes were made!')
-        elif(confirm.upper() == 'Y'):
-            setup_process = False
+    target_dir = '/media/Storage/CopyTest/Target'
+
+    files = os.listdir(args.source_dir)
+    for file in files:
+        full_path_src = os.path.join(args.source_dir, file)
+        full_path_target = os.path.join(args.target_dir, file)
+        if(os.path.isfile(full_path_src)):
+            try:
+                shutil.move(full_path_src, full_path_target)
+            except FileExistsError:
+                pass
+            except Exception as e:
+                print(repr(e))
 
     print('Script Finished!')
-
-def __target_select():
-    """
-    Takes the user input of which filesystem should be used as input or output.
-    :return: source, target
-    :rtype: list[str]
-    """
-    source = input("Select Source Device: ")
-    target = input("Select Target device: ")
-
-    return source, target
-
 
 if __name__ == '__main__':
     main()
